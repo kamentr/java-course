fbd8e7940b2bfad0539041977b6af55e4f0633ff
GeorgiZh <georgi03zh@gmail.com>
2023-06-05 19:24:06 +0300

    weather client

--- src/main/java/com/java/course/client/HistoricalWeatherClient.java
+++ src/main/java/com/java/course/client/HistoricalWeatherClient.java
@@ -1,7 +1,16 @@
 package com.java.course.client;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class HistoricalWeatherClient {
+    public String getWeather(double latitude, double longitude) {
+        String weatherUrl = "https://archive-api.open-meteo.com/v1/archive?latitude=" + latitude + "&longitude="+ longitude + "&start_date=2023-05-17&end_date=2023-05-31&hourly=temperature_2m";
+        ResponseEntity<String> response = restTemplate.getForEntity(weatherUrl, String.class);
 
+        return response.getBody();
+    }
 
+    RestTemplate restTemplate = new RestTemplate();
 
 }
